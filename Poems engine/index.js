a2f57880abc05f7d15dfb36b031c7b7bac59af15
matlo <write@matlo.me>
2021-08-23 23:39:54 +0100

    canvas

--- Poems engine/index.js
+++ Poems engine/index.js
@@ -1,6 +1,10 @@
 const fs = require("fs")
 const path = require("path")
 const csv = require("fast-csv")
+const p5 = require("node-p5")
+
+const cbook = p5.loadFont({ path: "./fonts/cbook.otf", family: "CBook" })
+const cbold = p5.loadFont({ path: "./fonts/cbold.otf", family: "CBold" })
 
 const jan = []
 const jul = []
@@ -20,26 +24,30 @@ fs.createReadStream(path.resolve(__dirname, "csv", "jan.csv"))
 
 const randomLine = (lines) => {
   const line = lines[~~(Math.random() * lines.length)]
-  const {
-    Date,
-    ClorofA,
-    ClorofTens,
-    CondSpec,
-    LDO,
-    LDOPerc,
-    pH,
-    Redox,
-    TempAcqua,
-  } = line
-  console.log(
-    Date,
-    ClorofA,
-    ClorofTens,
-    CondSpec,
-    LDO,
-    LDOPerc,
-    pH,
-    Redox,
-    TempAcqua
-  )
+  const { fullDate, ClorofA, pH, TempAcqua } = line
+  const date = new Date(fullDate)
+  const sentences = [
+    `Questa fu scritta il giorno ${date.getDate()} ${
+      date.getMonth() === 0 ? "Gennnaio" : "Agosto"
+    } alle ${date.getHours()}:${date.getMinutes()}. La temperatura`,
+    `era di ${TempAcqua}°, mentre la Clorofilla A era ${ClorofA}ug/l e il pH ${pH}. `,
+  ]
+
+  function sketch(p) {
+    p.setup = () => {
+      let canvas = p.createCanvas(639, 1772)
+      setTimeout(() => {
+        p.saveCanvas(canvas, "myCanvas", "png").then((filename) => {
+          console.log(`saved the canvas as ${filename}`)
+        })
+      }, 100)
+    }
+    p.draw = () => {
+      p.background(50)
+      p.textFont(cbold, 36)
+      p.text("hello world!", 50, 100)
+    }
+  }
+
+  let p5Instance = p5.createSketch(sketch)
 }
